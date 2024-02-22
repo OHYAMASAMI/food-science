790c998d74d56c439035b91cf08da2339f98c882
OHYAMASAMI <hellonanamomo@gmail.com>
2024-02-22 21:14:12 +0900

    2/22

--- functions.php
+++ functions.php
@@ -1,8 +1,19 @@
 <?php
 /** 
-*titleタグを出力する
+*テーマの機能を追加する
+@return void
 */
+function my_theme_support()
+{
+    //titleのタグを出力する
 add_theme_support('title-tag');
+
+
+
+//アイキャッチ画像を有効化する
+add_theme_support('post_thumbnails');
+}
+add_action('after_setup_theme' , 'my_theme_support');
 
 function my_document_title_separator($separator){
 $separator = '|';
